be6520189d71cd2b0c728e8139f7213d0c4afb24
Maxine Aubrey <maxeaubrey@gmail.com>
2021-09-25 18:10:42 +0200

    libsoup: 2.72.0 -> 2.74.0
    - https://gitlab.gnome.org/GNOME/libsoup/-/blob/2.74.0/NEWS

--- pkgs/development/libraries/libsoup/default.nix
+++ pkgs/development/libraries/libsoup/default.nix
@@ -23,20 +23,12 @@
 
 stdenv.mkDerivation rec {
   pname = "libsoup";
-  version = "2.72.0";
+  version = "2.74.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "11skbyw2pw32178q3h8pi7xqa41b2x4k6q4k9f75zxmh8s23y30p";
+    sha256 = "sha256-M7HU4NY5RWxnXCJ4d+lKgHjXMSM+LVdonBGrzvfTxI4=";
   };
-
-  patches = [
-    (fetchpatch {
-      # https://gitlab.gnome.org/GNOME/libsoup/-/issues/222
-      url = "https://gitlab.gnome.org/GNOME/libsoup/commit/b5e4f15a09d197b6a9b4b2d78b33779f27d828af.patch";
-      sha256 = "1hqk8lqzc200hi0nwbwq9qm6f03z296cnd79d4ql30683s80xqws";
-    })
-  ];
 
   postPatch = ''
     patchShebangs libsoup/
